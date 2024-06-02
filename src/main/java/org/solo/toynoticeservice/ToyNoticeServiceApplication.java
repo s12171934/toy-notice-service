0451afb5427bf71950802894a554564c65234d68
s12171934 <ssong99999@naver.com>
2024-06-02 23:30:59 +0900

    init

---
+++ src/main/java/org/solo/toynoticeservice/ToyNoticeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.solo.toynoticeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyNoticeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyNoticeServiceApplication.class, args);
+    }
+
+}
